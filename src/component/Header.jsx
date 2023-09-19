e7026f00ea6a2e83139024e653ca38b879d64d13
CMarchandon <camillemarchandon3@hotmail.fr>
2023-09-19 11:12:04 +0200

    assets

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -4,14 +4,16 @@ export default function Header() {
         <header>
             <nav>
                 <ul>
-                    <li><a href="/Home"><img src="public/logo.svg" alt="My Cooking Book Logo"/></a></li>
-                    <li><a href="/Discover">Discover</a></li>
-                    <li><a href="/About">About</a></li>
-                    <li><a href="/CreateRecipe" id="new-recipe-btn">Add a new recipe</a></li>
-                </ul> 
-                <ul>
-                    <li>navbar</li>
-                    <li><a href="/MyProfil">My profil</a></li>
+                    <div class= "left-items">
+                        <li><a href="/Home"><img src="public/logo.svg" alt="My Cooking Book Logo"/></a></li>
+                        <li><a href="/Discover">Discover</a></li>
+                        <li><a href="/About">About</a></li>
+                        <li><a href="/CreateRecipe" id="new-recipe-btn">Add a new recipe</a></li>
+                    </div>
+                    <div class="right-items">    
+                        <li>navbar</li>
+                        <li><a href="/MyProfil">My profil</a></li>
+                    </div>
                 </ul>
             </nav>
         </header>
